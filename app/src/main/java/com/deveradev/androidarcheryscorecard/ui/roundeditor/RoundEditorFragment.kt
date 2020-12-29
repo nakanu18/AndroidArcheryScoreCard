5d6fdca51e5a8fb3db3504e2a77006bfa58a87b9
Alex de Vera <adever2@gmail.com>
2020-12-28 22:26:24 -0600

    RoundEditor: add back button functionality

--- app/src/main/java/com/deveradev/androidarcheryscorecard/ui/roundeditor/RoundEditorFragment.kt
+++ app/src/main/java/com/deveradev/androidarcheryscorecard/ui/roundeditor/RoundEditorFragment.kt
@@ -1,12 +1,15 @@
 package com.deveradev.androidarcheryscorecard.ui.roundeditor
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.deveradev.androidarcheryscorecard.R
 import com.deveradev.androidarcheryscorecard.data.HistoryViewModel
 import com.deveradev.androidarcheryscorecard.data.HistoryViewModelFactory
@@ -14,12 +17,16 @@ import com.deveradev.androidarcheryscorecard.data.HistoryViewModelFactory
 class RoundEditorFragment : Fragment() {
 
     private lateinit var historyViewModel: HistoryViewModel
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val viewModelFactory = HistoryViewModelFactory(requireActivity())
+
+        this.navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment)
+        setHasOptionsMenu(true)
 
         this.historyViewModel = ViewModelProvider(requireActivity(), viewModelFactory).get(HistoryViewModel::class.java)
         this.historyViewModel.rounds.observe(this.viewLifecycleOwner, Observer {
@@ -28,6 +35,13 @@ class RoundEditorFragment : Fragment() {
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_round_editor, container, false)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            this.navController.navigateUp()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
